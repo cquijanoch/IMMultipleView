fc9b810fe5436313bcd7c2afe2aae139b8de6ec6
cquijanoch <cquijanoch@github.com>
2019-04-22 20:44:36 -0300

    fix change of current select space

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -6,15 +6,19 @@ using Valve.VR;
 public class Hand : MonoBehaviour
 {
     public SteamVR_Action_Boolean m_GrabAction = null;
+    public SteamVR_Action_Boolean m_GripAction = null;
 
     private SteamVR_Behaviour_Pose m_Pose = null;
     private FixedJoint m_Joint = null;
 
     private Interactable m_CurrentInteractable = null;
     public List<Interactable> m_ContactInteractables = new List<Interactable>();
-    private int m_currentIndexSelected = 0;
+    private int m_currentIndexSelected = -1;
 
     public float m_moveSped = 10f;
+
+    private Color m_colorSpaceSelected = new Color(0.6666527f, 0.6581524f, 0.1f, 0.2941177f);
+    private Color m_colorSpaceWithoutSelected = new Color(0.6666527f, 0.6581524f, 0.9622641f, 0.2941177f);
 
     private void Awake()
     {
@@ -28,19 +32,51 @@ public class Hand : MonoBehaviour
 
     void Update()
     {
-        //Down
+
         if(m_GrabAction.GetStateDown(m_Pose.inputSource))
         {
-            print(m_Pose.inputSource + " Trigger Down");
+            print(m_Pose.inputSource + " Trigger Grab Down");
             Pickup();
         }
 
-        //Up
-        if(m_GrabAction.GetStateUp(m_Pose.inputSource))
+        if (m_ContactInteractables.Count > 1 && SteamVR_Actions._default.GrabGrip.GetStateDown(m_Pose.inputSource))
         {
-            print(m_Pose.inputSource + "Trigger Up");
+            print(m_Pose.inputSource + " Trigger Grip Down");
+            ChangeCurrentSelectionSpace();
+        }
+
+        if (m_GrabAction.GetStateUp(m_Pose.inputSource))
+        {
+            print(m_Pose.inputSource + "Trigger Grab Up");
             Drop();
         }
+
+        if (m_CurrentInteractable && SteamVR_Actions._default.TouchXbutton.GetStateDown(m_Pose.inputSource) && m_ContactInteractables.Count > 1)
+        {
+            print(m_Pose.inputSource + "XButton Down");
+            SetTransformForSimilar();
+        }
+
+    }
+
+    private void SetTransformForSimilar()
+    {
+        Interactable target = m_currentIndexSelected + 1 == m_ContactInteractables.Count ? m_ContactInteractables[0] : m_ContactInteractables[m_currentIndexSelected + 1];
+        if (m_CurrentInteractable.DetectSimimilarTransform(target))
+        {
+            m_CurrentInteractable.SetTransformToObject(target);
+        }
+    }
+
+
+    private void ChangeCurrentSelectionSpace()
+    {
+        m_ContactInteractables[m_currentIndexSelected].GetComponent<Renderer>().material.color = m_colorSpaceWithoutSelected;
+        if ( ++m_currentIndexSelected == m_ContactInteractables.Count)
+        {
+            m_currentIndexSelected = 0;
+        }
+        m_ContactInteractables[m_currentIndexSelected].GetComponent<Renderer>().material.color = m_colorSpaceSelected;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -48,7 +84,9 @@ public class Hand : MonoBehaviour
         if (!other.gameObject.CompareTag("Interactable"))
             return;
         m_ContactInteractables.Add(other.gameObject.GetComponent<Interactable>());
-        m_ContactInteractables[m_currentIndexSelected].GetComponent<Renderer>().material.color = new Color(0.6666527f, 0.6581524f, 0.1f, 0.2941177f);
+        if (m_currentIndexSelected < 0) m_currentIndexSelected = 0;
+        m_ContactInteractables[m_currentIndexSelected].GetComponent<Renderer>().material.color = m_colorSpaceSelected;
+
 
     }
 
@@ -57,24 +95,27 @@ public class Hand : MonoBehaviour
         if (!other.gameObject.CompareTag("Interactable"))
             return;
         m_ContactInteractables.Remove(other.gameObject.GetComponent<Interactable>());
-        other.GetComponent<Renderer>().material.color = new Color(0.6666527f, 0.6581524f, 0.9622641f, 0.2941177f);
+        other.GetComponent<Renderer>().material.color = m_colorSpaceWithoutSelected;
+        if ( m_currentIndexSelected + 1 > m_ContactInteractables.Count )
+        {
+            m_currentIndexSelected--;
+        }
         if (m_ContactInteractables.Count > 0)
-            m_ContactInteractables[m_currentIndexSelected].GetComponent<Renderer>().material.color = new Color(0.6666527f, 0.6581524f, 0.1f, 0.2941177f);
+            m_ContactInteractables[m_currentIndexSelected].GetComponent<Renderer>().material.color = m_colorSpaceSelected;
     }
 
     public void Pickup()
     {
-        m_CurrentInteractable = GetNearestInteractable();
+        if (m_currentIndexSelected < 0 )
+            return;
+        m_CurrentInteractable = m_ContactInteractables[m_currentIndexSelected];
         if (!m_CurrentInteractable)
             return;
         if (m_CurrentInteractable.m_ActiveHand)
             m_CurrentInteractable.m_ActiveHand.Drop();
 
-        //m_CurrentInteractable.transform.position = transform.position;
-
         Rigidbody targetBody = m_CurrentInteractable.GetComponent<Rigidbody>();
         m_Joint.connectedBody = targetBody;
-        
 
         m_CurrentInteractable.m_ActiveHand = this;
     }
--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Interactable : MonoBehaviour
 {
+    public float m_minDistance = 0.5f;
+    
+    [HideInInspector]
+    public Hand m_ActiveHand = null;
+
     private void Start()
     {
         Physics.IgnoreCollision(GameObject.FindGameObjectWithTag("Plane").GetComponent<Collider>(), GetComponent<Collider>());
         foreach(GameObject gm in GameObject.FindGameObjectsWithTag("Interactable"))
             Physics.IgnoreCollision(gm.GetComponent<Collider>(), GetComponent<Collider>());
     }
-    [HideInInspector]
-    public Hand m_ActiveHand = null;
+
+    private void Update()
+    {
+          
+    }
+
+    public bool DetectSimimilarTransform(Interactable other)
+    {
+        print(Vector3.Distance(transform.position, other.gameObject.transform.position));
+        if (Vector3.Distance(transform.position, other.gameObject.transform.position) <= m_minDistance)
+            return true;
+        return false;
+    }
+
+    public void SetTransformToObject(Interactable obj)
+    {
+        transform.position = obj.transform.position;
+    }
 
 }
