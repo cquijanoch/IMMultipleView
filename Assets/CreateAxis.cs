99c58873e8f5889cde6cc41805777ab0c53e338d
cquijanoch <cquijanoch@github.com>
2019-05-14 19:47:54 -0300

    axis line and laber for bar plot

--- Assets/CreateAxis.cs
+++ Assets/CreateAxis.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections;
+﻿
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CreateAxis : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
